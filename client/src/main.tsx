da3198eb67cbcb8b0c62d159eae3b0ef6861279a
d-liya <dilum14@gmail.com>
2025-07-16 16:13:58 -0230

    refactor: implement routing for the main application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
